5107bdd133883dd02225db86e6e1ee717dbdf225
d22samma <d22samma@student.his.se>
2023-05-09 15:22:08 +0200

    Update Birds.java Added Getter,Setters and constructor

--- app/src/main/java/com/example/myproject/Birds.java
+++ app/src/main/java/com/example/myproject/Birds.java
@@ -8,4 +8,45 @@ public class Birds {
     @SerializedName("category")
     private String size;
     private String location;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+    public Birds(String name, String company, String size, String location) {
+        this.name = name;
+        this.company = company;
+        this.size = size;
+        this.location = location;
+    }
 }
